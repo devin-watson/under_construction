6bd44d57a71be7f8e8c370248dc2602dbea03d04
Devin <devinwatson015@gmail.com>
2023-12-30 19:42:38 +1100

    remove twitter link and jquery

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-$(document).ready(function() {
-	var randomNumber = Math.floor(Math.random() * Math.floor(4));
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const randomNumber = Math.floor(Math.random() * Math.floor(4));
+    const chef = document.querySelector(".chefWrapper");
+    const construction = document.querySelector(".constructionWrapper");
+    const rider = document.querySelector(".riderWrapper");
+    const pirate = document.querySelector(".pirateWrapper");
 
-	if(randomNumber == 0) {
-		$(".chefWrapper").show();
-		$(".constructionWrapper").hide();
-		$(".riderWrapper").hide();
-		$(".pirateWrapper").hide();
-	} else if(randomNumber == 1) {
-		$(".constructionWrapper").show();
-		$(".chefWrapper").hide();
-		$(".riderWrapper").hide();
-		$(".pirateWrapper").hide();
-	} else if(randomNumber == 2) {
-		$(".constructionWrapper").hide();
-		$(".chefWrapper").hide();
-		$(".riderWrapper").show();
-		$(".pirateWrapper").hide();
-	} else {
-		$(".constructionWrapper").hide();
-		$(".chefWrapper").hide();
-		$(".riderWrapper").hide();
-		$(".pirateWrapper").show();
-	}
-})
+    const possibleImages = [chef, construction, rider, pirate];
+
+    possibleImages.forEach((image) => {
+      const imageRandomId = image.getAttribute("data-random-id");
+      console.log({ imageRandomId, randomNumber });
+      if (parseInt(imageRandomId) === randomNumber) {
+        image.toggleAttribute("data-visible", true);
+      } else {
+        image.toggleAttribute("data-visible", false);
+      }
+    });
+  },
+  false
+);
